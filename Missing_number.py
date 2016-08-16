864be39aeac0bcf6a231e20c60d1f61b5302fb92
root <root@localhost.localdomain>
2016-08-16 10:20:36 +0300

    first commit

---
+++ Missing_number.py
@@ -0,0 +1,7 @@
+def find_missing(a,b):
+    if len([c for c in b if c not in a])>0:
+        p=[c for c in b if c not in a]
+        return p[0]
+    else:
+        return 0
+find_missing([1, 2], [1, 2, 5])
